fd40607c42736b8d2d534c93e448d52df7c7d247
8luebottle <itbiz.irs@gmail.com>
2020-02-14 17:21:43 +0900

    home_controller
    Home Method

---
+++ api/controllers/home_controller.go
@@ -0,0 +1,12 @@
+package controllers
+
+import (
+	"net/http"
+
+	"github.com/8luebottle/go-blog/api/responses"
+)
+
+// Welcome API User
+func (server *Server) Home(w http.ResponseWriter, r *http.Request) {
+	responses.JSON(w, http.StatusOK, "welcome to this Awesome API")
+}
