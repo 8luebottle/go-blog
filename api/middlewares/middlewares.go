3f70ee4e3ed9fe94d1377065978704028d07d4d0
8luebottle <itbiz.irs@gmail.com>
2020-02-14 14:18:22 +0900

    middlewares
    SetMiddlewareJSONN(), SetMiddlewareAuthentication()

---
+++ api/middlewares/middlewares.go
@@ -0,0 +1,27 @@
+package middlewares
+
+import (
+	"errors"
+	"net/http"
+
+	"github.com/8luebottle/go-blog/api/auth"
+	"github.com/8luebottle/go-blog/api/responses"
+)
+
+func SetMiddlewareJSON(next http.HandlerFunc) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "application/json")
+		next(w, r)
+	}
+}
+
+func SetMiddlewareAuthentication(next http.HandlerFunc) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		err := auth.ValidToken(r)
+		if err != nil {
+			responses.ERROR(w, http.StatusUnauthorized, errors.New("unauthorized"))
+			return
+		}
+		next(w, r)
+	}
+}
