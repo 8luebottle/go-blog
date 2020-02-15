2552cecf288bd293e8cbdd4b61f6d2312b8e1a30
8luebottle <itbiz.irs@gmail.com>
2020-02-15 16:05:52 +0900

    main.go
    Entry Point (api.Run())

--- main.go
+++ main.go
@@ -1,14 +1,7 @@
 package main
 
-import (
-	"fmt"
-	"github.com/8luebottle/go-blog/api/models"
-)
+import "github.com/8luebottle/go-blog/api"
 
 func main() {
-	user := models.User{}
-	if err := user.Validate("aa"); err != nil {
-		fmt.Println(err)
-	}
-	fmt.Print("asdasd")
+	api.Run()
 }
